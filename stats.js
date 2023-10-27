8a0b21ff1685dbe431b2811a4bfb2c6adbb56c2c
Mateus Roriz <mateusvroriz6b@gmail.com>
2023-10-27 11:25:52 -0300

    adicionando funcoes para calculo

--- stats.js
+++ stats.js
@@ -1,10 +1,14 @@
 const os = require('os');
 const { freemem, totalmem } = os;
+const { Mono } = require('reactor-core');
+const { Flux } = require('reactor-core');
+const { Operator } = require('reactor-core');
 
 const total = parseInt(totalmem() / 1024 / 1024);
 const mem = parseInt(freemem() / 1024 / 1024);
 const percents = parseInt((mem / total) * 100);
 const miliseconds = 5000;
+const conditional = true;
 
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -12,7 +16,7 @@ function sleep(ms) {
 
 
 async function pcStats() {
-    while(1){
+    while(conditional){
         await sleep(miliseconds);
         const stats = {
             "free_memory": `${mem} MB`,
@@ -25,5 +29,18 @@ async function pcStats() {
         console.table(stats);
     };
 };
+
+
+// Função que representa a curva
+function curve(x) {
+  return x ** 2; 
+}
+
+// Função para calcular a derivada
+function calculateDerivative(x) {
+  return Flux.just(x) // Crie um fluxo com o valor de x
+    .map(value => (f(value + 0.0001) - f(value)) / 0.0001) // Aproximação da derivada
+    .next(); // Obtenha o valor calculado
+}
 
 pcStats();
