7ed6561e14fd4edf8711b85704724323cd5d1cc3
Mateus Roriz <mateusvroriz6b@gmail.com>
2023-10-27 10:26:45 -0300

    tirando variaveis magicas do projeto

--- stats.js
+++ stats.js
@@ -4,6 +4,7 @@ const { freemem, totalmem } = os;
 const total = parseInt(totalmem() / 1024 / 1024);
 const mem = parseInt(freemem() / 1024 / 1024);
 const percents = parseInt((mem / total) * 100);
+const miliseconds = 5000;
 
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -12,7 +13,7 @@ function sleep(ms) {
 
 async function pcStats() {
     while(1){
-        await sleep(5000);
+        await sleep(miliseconds);
         const stats = {
             "free_memory": `${mem} MB`,
             "total_memory": `${total} MB`,
